2f49fb90cbb030dd4ce160708e6c9d6aa54f21af
mahmoud-salameh <mahmoud4561@hotmail.com>
2021-09-13 20:17:36 +0300

    solving lab37 befor the PR

--- pages/index.js
+++ pages/index.js
@@ -1,82 +1,91 @@
-import Head from 'next/head'
+import Head from 'next/head';
+import { useState } from 'react';
 
 export default function Home() {
+  const [data, setData] = useState([]);
+
+  function submitForm(event) {
+    event.preventDefault();
+    const answeredQuestion = {
+      Location: event.target.Location.value,
+      Minimum: event.target.Minimum.value,
+      Maximum: event.target.Maximum.value,
+      Average: event.target.Average.value,
+    };
+    setData((answers) => [...answers, answeredQuestion]);
+  }
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+    <div className="">
       <Head>
-        <title>Create Next App</title>
+        <title>Cookie Stand Admin</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
+      <main className=" bg-green-50">
 
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
-            pages/index.js
-          </code>
-        </p>
 
-        <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
+        <header className='bg-green-500 p-6'>
 
-          <a
-            href="https://nextjs.org/learn"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
+        <h1 className='text-5xl'>Cookie Stand Admin</h1>
 
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
+        </header>
 
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        <form className="flex-col w-3/4 p-2 mx-auto my-8 bg-green-300" onSubmit={submitForm}>
+        <fieldset>  
+          <h1 className="my-3 text-2xl text-center" type ='text'>Cookie Stand Admin</h1>  
+          <div className="flex">
+          <label className="mx-1">Location</label>
+          <input name="Location"  className="flex-auto bg-gray-100" />
+          </div>
+          <div className="flex w-3/4 my-5" >
+          <div className="flex-col w-1/4">
+         <label >Minimum Custumer per Hour</label>
+          <input type='integer' className="" name="Minimum" />
+         </div>
+         <div className="flex-col w-1/4">
+          <label >Maximum Custumer per Hour</label>
+          <input type='integer'  className=""name="Maximum" />
+          </div>
+          <div className="flex-col w-1/4">
+            <div className="flex"></div><label >Average Cookies per Sales</label>
+          <input type='integer'  className="" name="Average" />
+          </div>
+          <button className="px-20 py-5 bg-green-500 text-gray-50">Create</button>
+          </div>
+        </fieldset>  
+        </form>
+
+        <table className="w-1/2 mx-auto my-4">
+          <thead>
+            <tr>
+              <th className="">location</th>
+              <th className="">minCustomers</th>
+              <th className="">maxCustomers</th>
+              <th className="">Average</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((i) => (
+              <tr>
+                <td className="pl-2 border border-gray-700">
+                  {i.Location}
+                </td>
+                <td className="pl-2 border border-gray-700">
+                  {i.Minimum}
+                </td>
+                <td className="pl-2 border border-gray-700">
+                  {i.Maximum}
+                </td>
+                <td className="pl-2 border border-gray-700">
+                  {i.Average}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+
+      <footer className="bg-green-500 p-6 text-3xl">&copy;2021</footer>
       </main>
-
-      <footer className="flex items-center justify-center w-full h-24 border-t">
-        <a
-          className="flex items-center justify-center"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
-        </a>
-      </footer>
     </div>
-  )
+  );
 }
